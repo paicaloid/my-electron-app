7cc1512db5aa8ec575150765981fc6ab2bf4fcfa
paicaloid <paicaloid@gmail.com>
2024-04-26 18:21:27 +0700

    Add asset from example

--- main.js
+++ main.js
@@ -1,26 +1,53 @@
-const { BrowserWindow, app } = require('electron');
-const path = require('path');
-const isMac = process.platform === 'darwin';
+const { BrowserWindow, app, Menu } = require('electron')
+const path = require('path')
 
+const isDev = process.env.NODE_ENV !== 'development'
+const isMac = process.platform === 'darwin'
+
+// Create main window
 function createMainWindow() {
   const mainWIndow = new BrowserWindow({
     title: 'Yeah!',
-    width: 500,
-    height: 600,
-  });
+    width: isDev ? 1000 : 500,
+    height: 800,
+  })
 
-  mainWIndow.loadFile(path.join(__dirname, './renderer/index.html'));
+  // Open DevTools in development mode
+  if (isDev) {
+    mainWIndow.webContents.openDevTools()
+  }
+
+  mainWIndow.loadFile(path.join(__dirname, './renderer/index.html'))
 }
 
+// App is ready
 app.whenReady().then(() => {
-  createMainWindow();
+  createMainWindow()
+
+  // Implement menu
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createMainWindow()
     }
-  });
-});
+  })
+})
+
+// Menu template
+const menu = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        click: () => app.quit(),
+        accelerator: 'CmdOrCtrl+W',
+      },
+    ],
+  },
+]
 
 app.on('window-all-closed', () => {
   if (!isMac) {
